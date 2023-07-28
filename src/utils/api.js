c0e378bee6c687404fbad4aaf8ca24c63648370f
dennisobel <ogembodenis2016@gmail.com>
2023-07-28 16:31:18 +0300

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -23,11 +23,7 @@ export const fetchDataFromApi = async (url, params) => {
 
 export const fetchGames = async () => {
     try {
-<<<<<<< HEAD
-        const data = await axios.get("http://34.241.183.143:8080/games")
-=======
         const data = await axios.get(`http://34.241.183.143:8080/games`)
->>>>>>> 6ee623581e375176276872422f5140f42b0aa45a
         return data
     } catch (error) {
         return error
