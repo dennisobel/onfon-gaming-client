2a4e594226d20243efcb151130ccb5567b89dc22
dennisobel <ogembodenis2016@gmail.com>
2023-08-03 15:55:57 +0300

    cahnges

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -121,12 +121,14 @@ const DetailsBanner = ({ video, crew }) => {
         console.log(dets?.homepage)
         // const newWindow = window.open(`http://34.241.183.143:5000/${dets?.homepage}`, '_blank', 'noopener,noreferrer,location=no');
         // const newWindow = window.open(`http://api.epicgames.co.ke:5000/${dets?.homepage}`, '_blank', 'noopener,noreferrer,location=no');
-        const newWindow = window.open(`http://api.epicgames.co.ke/${dets?.homepage}`, '_blank', 'noopener,noreferrer,location=no');
+        // const newWindow = window.open(`http://api.epicgames.co.ke/${dets?.homepage}`, 'noopener,noreferrer,location=no');
         // const newWindow = window.open(`http://games.somwaki.com:5000/${dets?.homepage}`, '_blank', 'noopener,noreferrer,location=no');
         // const newWindow = window.open(`https://api.epicgames.co.ke/9`, '_blank', 'noopener,noreferrer,location=no');
         // const newWindow = window.open(`https://google.com`, '_blank', 'noopener,noreferrer,location=no');
         // https://api.epicgames.co.ke/
-        newWindow.opener = null; 
+        // newWindow.opener = null; 
+        const newURL = `http://api.epicgames.co.ke/${dets?.homepage}`;
+        window.location.href = newURL;
     }
 
     return (
