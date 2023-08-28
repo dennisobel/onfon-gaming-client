9dbf7214c0ddf8388d94bc6b513e4e9ca171437f
dennisobel <ogembodenis2016@gmail.com>
2023-08-28 09:31:10 +0300

    cleanup

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -3,20 +3,17 @@ import React, { useEffect, useState } from "react";
 import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
-import useFetch from "../../../hooks/useFetch";
 
 import { useSelector } from "react-redux";
 
 import { fetchGames } from "../../../utils/api";
 
 const Trending = () => {
-    const [endpoint, setEndpoint] = useState("day");
     const [filtered, setFiltered] = useState()
     const { query } = useSelector((state) => state.home);
 
     const [games,setGames] = useState()
 
-    // const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
 
     const getGames = () => {
         fetchGames().then(res => setGames(res))
@@ -42,7 +39,6 @@ const Trending = () => {
         <div className="carouselSection">
             <ContentWrapper>
                 <span className="carouselTitle">Games</span>
-                {/* <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} /> */}
             </ContentWrapper>
             <Carousel games={res} />
         </div>
