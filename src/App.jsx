b971bec4e3738074680fc07fddfbc3b8839da0de
dennisobel <ogembodenis2016@gmail.com>
2023-08-29 13:50:56 +0300

    siteLoad

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,19 @@ import { gameRegister } from "./utils/api";
 
 import { setParsed } from "./store/homeSlice";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
     const dispatch = useDispatch();
     const [modalOpen, setModalOpen] = useState(false)
     const [connectionType, setConnectionType] = useState('unknown');
-    // const [parsed,setParsed] = useState({})
+    const [headerdata, setHeaderdata] = useState({})
+    const [modalText, setModalText] = useState()
 
 
-    const handleModalOpen = () => {
+    const handleModalOpen = (text) => {
+        setModalText(text)
         setModalOpen(true)
     }
 
@@ -33,26 +38,21 @@ function App() {
     }
 
     useEffect(() => {
-        console.log("navigation:",navigator.userAgent.toLowerCase())
-        fetchData();
-    }, []);
-
-    useEffect(() => {
         const updateConnectionType = () => {
             const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
-            
+
             if (connection) {
                 const type = connection.type;
-                
+
                 setConnectionType(type);
             }
         };
 
-        if(!navigator.userAgent.toLowerCase().includes("mozilla")){
+        if (!navigator.userAgent.toLowerCase().includes("mozilla")) {
             updateConnectionType();
 
             navigator.connection.addEventListener('change', updateConnectionType);
-    
+
             return () => {
                 navigator.connection.removeEventListener('change', updateConnectionType);
             };
@@ -62,30 +62,90 @@ function App() {
     useEffect(() => {
         // console.log("connection type:",connectionType)
         if (connectionType === "wifi") {
-            handleModalOpen()
+            handleModalOpen("Please switch to safaricom mobile data to continue.")
         } else if (connectionType === "cellular") {
         } else {
-            
+
         }
     }, [connectionType]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        // console.log("headerdata:", headerdata)
+        // checkSubscribed()
+        // subscribe()
+        siteLoad(headerdata)
+    }, [headerdata])
+
+    const checkSubscribed = async () => {
+        let { data } = await axios.post('http://sub.epicgames.co.ke/check-subscribe', { msisdn: "254726354124" }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log("Subscribed:", data)
+        if (data.Subscribed === 1) {
+            toast.success("You are subscribed")
+        } else if (data.Subscribed === 0) {
+            handleModalOpen(`You are about to subscribe to Onfon Gaming Service. This service charges ksh 10 per day.
+            To activate the service enter 1 on your phone`)
+            subscribe()
+        }
+    }
+
+    const subscribe = async () => {
+
+        let res = await axios.post("http://sub.epicgames.co.ke/subscribe", {
+            msisdn: "254727677068",
+            ip_address: headerdata.ip,
+            command: "subscribe",
+            channel: "epicgames"
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    const siteLoad = async (data) => {
+        const site_load = await axios.post(
+            `https://dashboard.epicgames.co.ke/game_play`,
+            data,
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        console.log("site_load:", site_load)
+    }
 
     const fetchData = async () => {
         const res = await axios.get("https://api.ipify.org/?format=json");
         let headerRes = await fetch("https://header.safaricombeats.co.ke/").then(res => res.text()).catch(err => console.log("err:", err))
         const parsedData = new XMLParser().parseFromString(headerRes);
         parsedData["ip"] = res.data.ip;
+        setHeaderdata(parsedData)
         dispatch(setParsed(JSON.stringify(parsedData)));
-        // setParsed(parsedData)
-        console.log("parsedData:", parsedData)
-        console.log(parsedData.children[0].children[0].children[0].children[1].value)
-        if(connectionType === "wifi" || parsedData.children[0].children[0].children[0].children[1].value === "999"){
-            handleModalOpen()
+
+        // console.log("parsedData:", parsedData)
+        // console.log(parsedData.children[0].children[0].children[0].children[1].value)
+        if (connectionType === "wifi" || parsedData.children[0].children[0].children[0].children[1].value === "999") {
+            handleModalOpen("Please switch to safaricom mobile data to continue.")
         }
-        gameRegister(parsedData).then().catch(err => console.log("err:", err));
+        // await gameRegister(parsedData).then().catch(err => console.log("err:", err));
+
+
+        console.log("site_load:", site_load)
     };
 
     return (
         <>
+            <ToastContainer />
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -106,7 +166,7 @@ function App() {
                     </DialogTitle>
                     <DialogContent>
                         <DialogContentText id="alert-dialog-description">
-                            Please switch to safaricom mobile data to continue.
+                            {modalText}
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
