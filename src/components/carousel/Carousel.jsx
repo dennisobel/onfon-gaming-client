3f28c74d3d3be1fed4cd8982ed4fc7bba4e8b201
dennisobel <ogembodenis2016@gmail.com>
2023-08-30 10:20:35 +0300

    WIP

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -127,11 +127,12 @@ const Carousel = ({ title, games, data }) => {
                                                     console.log("patternNumber:", patternNumber)
                                                     if (patternNumber === 1) {                                                        
                                                         // checkSubscribed({ msisdn: extractedValue, ip })
-                                                        checkSubscribed({ msisdn: extractedValue, ip:"8.8.8.8" })                 
+                                                        checkSubscribed({ msisdn: extractedValue, ip:"8.8.8.8" })
+                                                        console.log("sub1:", sub)           
                                                         // sub === true ? window.location.href = `https://api.epicgames.co.ke/${item?.homepage}/` : subscribe({ msisdn: extractedValue, ip })
                                                         sub === true ? window.location.href = `http://api.epicgames.co.ke/${item?.homepage}/` : subscribe({ msisdn: extractedValue, ip: "8.8.8.8" })
                                                     } else if (patternNumber === 2) {
-                                                        console.log("Prompt user to use cellular data:", extractedValue);
+                                                        console.log("sub2:", sub)           
                                                         handleModalOpen("Please switch to safaricom mobile data to continue.");
                                                     }
                                                 } else {
